a5606ac1cae29941af64c2501ca32265cf89f033
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-05-24 18:52:50 +0300

    OnConnected, OnError were added, minor changes

--- HftSocket.cs
+++ HftSocket.cs
@@ -33,6 +33,10 @@ namespace GridEx.API
 
 		public Action<HftSocket, Exception> OnException = delegate { };
 
+		public Action<HftSocket> OnConnected = delegate { };
+
+		public Action<HftSocket, SocketError> OnError = delegate { };
+
 		public Action<HftSocket> OnDisconnected = delegate { };
 
 		public HftSocket()
@@ -53,6 +57,13 @@ namespace GridEx.API
 			_tcpClient.Connect(endPoint);
 			_stream = _tcpClient.GetStream();
 			Thread.Sleep(TimeOutAfterConnect);
+
+			OnConnected(this);
+		}
+
+		public bool IsConnected
+		{
+			get { return _tcpClient.Connected; }
 		}
 
 		public void Send<TRequest>(TRequest request) where TRequest : struct, IHftRequest
@@ -78,7 +89,7 @@ namespace GridEx.API
 			}
 		}
 
-		public void Wait(CancellationToken cancellationToken)
+		public void WaitResponses(CancellationToken cancellationToken)
 		{
 			var responseBuffer = _byteArrayPool.Rent(ResponseSize.Max);
 			try
@@ -112,52 +123,7 @@ namespace GridEx.API
 						throw new IOException($"Wrong message bytes amount was received: '{responseBytesReceived}'.");
 					}
 
-					switch ((ResponseTypeCode)responseBuffer[1])
-					{
-						case ResponseTypeCode.OrderCreated:
-							ref readonly OrderCreated orderCreated = ref OrderCreated.CopyFrom(responseBuffer);
-							OnOrderCreated(this, orderCreated);
-							break;
-						case ResponseTypeCode.OrderExecuted:
-							ref readonly OrderExecuted orderExecuted = ref OrderExecuted.CopyFrom(responseBuffer);
-							OnOrderExecuted(this, orderExecuted);
-							break;
-						case ResponseTypeCode.OrderCancelled:
-							ref readonly OrderCancelled orderCancelled = ref OrderCancelled.CopyFrom(responseBuffer);
-							OnOrderCancelled(this, orderCancelled);
-							break;
-						case ResponseTypeCode.AllOrdersCancelled:
-							ref readonly AllOrdersCancelled allOrdersCancelled = ref AllOrdersCancelled.CopyFrom(responseBuffer);
-							OnAllOrdersCancelled(this, allOrdersCancelled);
-							break;
-						case ResponseTypeCode.UserTokenRejected:
-							ref readonly UserTokenRejected rejectedToken = ref UserTokenRejected.CopyFrom(responseBuffer);
-							OnUserTokenRejected(this, rejectedToken);
-							break;
-						case ResponseTypeCode.UserTokenAccepted:
-							ref readonly UserTokenAccepted acceptedToken = ref UserTokenAccepted.CopyFrom(responseBuffer);
-							OnUserTokenAccepted(this, acceptedToken);
-							break;
-						case ResponseTypeCode.OrderRejected:
-							ref readonly OrderRejected orderRejected = ref OrderRejected.CopyFrom(responseBuffer);
-							OnOrderRejected(this, orderRejected);
-							break;
-						case ResponseTypeCode.MarketInfo:
-							ref readonly MarketInfo marketInfo = ref MarketInfo.CopyFrom(responseBuffer);
-							OnMarketInfo(this, marketInfo);
-							break;
-						case ResponseTypeCode.ConnectionTooSlow:
-							ref readonly ConnectionTooSlow connectionTooSlow = ref ConnectionTooSlow.CopyFrom(responseBuffer);
-							OnConnectionTooSlow(this, connectionTooSlow);
-							break;
-						case ResponseTypeCode.RequestRejected:
-							ref readonly RequestRejected requestRejected = ref RequestRejected.CopyFrom(responseBuffer);
-							OnRequestRejected(this, requestRejected);
-							break;
-						default:
-							// unknown response
-							break;
-					}
+					CreateResponse(responseBuffer);
 				}
 			}
 			catch (Exception exception)
@@ -174,13 +140,70 @@ namespace GridEx.API
 		{
 			_stream.Close();
 			_tcpClient.Close();
+
+			OnDisconnected(this);
 		}
 
 		public void Dispose()
 		{
-			_tcpClient.Dispose();
+			if (Interlocked.CompareExchange(ref _isDisposed, 1, 0) == 0)
+			{
+				_tcpClient.Dispose();
+			}
 		}
 
+		private void CreateResponse(byte[] buffer)
+		{
+			switch ((ResponseTypeCode)buffer[1])
+			{
+				case ResponseTypeCode.OrderCreated:
+					ref readonly OrderCreated orderCreated = ref OrderCreated.CopyFrom(buffer);
+					OnOrderCreated(this, orderCreated);
+					break;
+				case ResponseTypeCode.OrderExecuted:
+					ref readonly OrderExecuted orderExecuted = ref OrderExecuted.CopyFrom(buffer);
+					OnOrderExecuted(this, orderExecuted);
+					break;
+				case ResponseTypeCode.OrderCancelled:
+					ref readonly OrderCancelled orderCancelled = ref OrderCancelled.CopyFrom(buffer);
+					OnOrderCancelled(this, orderCancelled);
+					break;
+				case ResponseTypeCode.AllOrdersCancelled:
+					ref readonly AllOrdersCancelled allOrdersCancelled = ref AllOrdersCancelled.CopyFrom(buffer);
+					OnAllOrdersCancelled(this, allOrdersCancelled);
+					break;
+				case ResponseTypeCode.UserTokenRejected:
+					ref readonly UserTokenRejected rejectedToken = ref UserTokenRejected.CopyFrom(buffer);
+					OnUserTokenRejected(this, rejectedToken);
+					break;
+				case ResponseTypeCode.UserTokenAccepted:
+					ref readonly UserTokenAccepted acceptedToken = ref UserTokenAccepted.CopyFrom(buffer);
+					OnUserTokenAccepted(this, acceptedToken);
+					break;
+				case ResponseTypeCode.OrderRejected:
+					ref readonly OrderRejected orderRejected = ref OrderRejected.CopyFrom(buffer);
+					OnOrderRejected(this, orderRejected);
+					break;
+				case ResponseTypeCode.MarketInfo:
+					ref readonly MarketInfo marketInfo = ref MarketInfo.CopyFrom(buffer);
+					OnMarketInfo(this, marketInfo);
+					break;
+				case ResponseTypeCode.ConnectionTooSlow:
+					ref readonly ConnectionTooSlow connectionTooSlow = ref ConnectionTooSlow.CopyFrom(buffer);
+					OnConnectionTooSlow(this, connectionTooSlow);
+					break;
+				case ResponseTypeCode.RequestRejected:
+					ref readonly RequestRejected requestRejected = ref RequestRejected.CopyFrom(buffer);
+					OnRequestRejected(this, requestRejected);
+					break;
+				default:
+					;
+					// unknown response
+					break;
+			}
+		}
+
+		private int _isDisposed = 0;
 		private readonly TcpClient _tcpClient;
 		private NetworkStream _stream;
 		private readonly ArrayPool<byte> _byteArrayPool = ArrayPool<byte>.Shared;
