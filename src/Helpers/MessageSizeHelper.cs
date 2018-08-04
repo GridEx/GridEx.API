a718fa0d9316ae4e064f4e975781d8d94bae13cc
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-08-04 22:07:45 +0300

    to search types for all assemblies in current domain

--- src/Helpers/MessageSizeHelper.cs
+++ src/Helpers/MessageSizeHelper.cs
@@ -1,22 +1,26 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace GridEx.API.Helpers
 {
-    public static class MessageSizeHelper
-    {
-	    public static void GetMinMax<T>(out int minSize, out int maxSize)
-	    {
-		    var responseSizes = typeof(T)
-			    .Assembly
-			    .GetTypes()
-			    .Where(type => typeof(T).IsAssignableFrom(type) && type.IsValueType)
-			    .Select(type => type.GetField("MessageSize", BindingFlags.Public | BindingFlags.Static))
-			    .Select(field => (ushort)field.GetValue(null));
+	public static class MessageSizeHelper
+	{
+		public static void GetMinMax<T>(out int minSize, out int maxSize)
+		{
+			var responseSizes = AppDomain
+				.CurrentDomain
+				.GetAssemblies()
+				.Select(assembly => assembly.GetTypes())
+				.SelectMany(t => t)
+				.Distinct()
+				.Where(type => typeof(T).IsAssignableFrom(type) && type.IsValueType)
+				.Select(type => type.GetField("MessageSize", BindingFlags.Public | BindingFlags.Static))
+				.Select(field => (ushort)field.GetValue(null));
 
-		    minSize = responseSizes.Min();
+			minSize = responseSizes.Min();
 
-		    maxSize = responseSizes.Max();
-	    }
+			maxSize = responseSizes.Max();
+		}
 	}
 }
