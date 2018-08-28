6693e4f3eeea9e9efd2ac4ccc09443cd3b2060b5
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-08-28 20:01:49 +0300

    formatting

--- src/VolumeRange.cs
+++ src/VolumeRange.cs
@@ -4,48 +4,48 @@ using System.Runtime.CompilerServices;
 namespace GridEx.API
 {
 	public static class VolumeRange
-    {
+	{
 		public static event Action<double, double> OnRangeChanged = delegate { };
 
-	    public static void Init(double min, double max)
-	    {
-		    if (min >= max)
-		    {
-			    throw new ArgumentOutOfRangeException(nameof(max));
-		    }
+		public static void Init(double min, double max)
+		{
+			if (min >= max)
+			{
+				throw new ArgumentOutOfRangeException(nameof(max));
+			}
 
-		    Min = min;
-		    Max = max;
+			Min = min;
+			Max = max;
 
-		    OnRangeChanged(Min, Max);
-	    }
+			OnRangeChanged(Min, Max);
+		}
 
-	    public static double Min
-	    {
-		    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-		    get;
-		    private set;
-	    }
+		public static double Min
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get;
+			private set;
+		}
 
-	    public static double Max
-	    {
-		    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-		    get;
-		    private set;
-	    }
+		public static double Max
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get;
+			private set;
+		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static bool InBuySideRange(double volume, double price)
-	    {
-		    var buySideVolume = volume * price;
+		{
+			var buySideVolume = volume * price;
 
-		    return Min <= buySideVolume && buySideVolume <= Max;
-	    }
+			return Min <= buySideVolume && buySideVolume <= Max;
+		}
 
-	    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static bool InBuySideRange(double buySideVolume)
-	    {
-		    return Min <= buySideVolume && buySideVolume <= Max;
-	    }
-    }
+		{
+			return Min <= buySideVolume && buySideVolume <= Max;
+		}
+	}
 }
