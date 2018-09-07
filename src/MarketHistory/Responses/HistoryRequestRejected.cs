535a26f1ef4d153d0075f063e1465f7b7f55f946
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-09-07 15:55:09 +0300

    HistoryRequestRejected was added, HistoryResponseTypeCode was changed

---
+++ src/MarketHistory/Responses/HistoryRequestRejected.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace GridEx.API.MarketHistory.Responses
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public readonly struct HistoryRequestRejected : IHistoryResponse
+	{
+		public HistoryRequestRejected(HistoryRejectReasonCode rejectCode)
+		{
+			Size = MessageSize;
+			TypeCode = HistoryResponseTypeCode.RequestRejected;
+			RejectCode = rejectCode;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static unsafe ref readonly HistoryRequestRejected CopyFrom(byte[] array, int offset = 0)
+		{
+			fixed (byte* source = &array[offset])
+			{
+				return ref ((HistoryRequestRejected*)source)[0];
+			}
+		}
+
+		public ushort Size
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get;
+		}
+
+		public HistoryResponseTypeCode TypeCode
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get;
+		}
+
+		public readonly HistoryRejectReasonCode RejectCode;
+
+		public static readonly ushort MessageSize = Convert.ToUInt16(Marshal.SizeOf<HistoryRequestRejected>());
+	}
+}
