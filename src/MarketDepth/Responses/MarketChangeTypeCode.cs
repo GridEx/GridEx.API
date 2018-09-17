4210cfbd092fd8fd059242e05864626f64094717
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-09-17 12:12:28 +0300

    minor renaming

--- src/MarketDepth/Responses/MarketChangeTypeCode.cs
+++ src/MarketDepth/Responses/MarketChangeTypeCode.cs
@@ -7,14 +7,14 @@
 		BidPriceByCanceledOrder = 12,		// Level I, Level II, Level III
 		BidVolumeByExecutedOrder = 13,		// Level I, Level II, Level III
 		BidVolumeByAddedOrder = 14,			// Level I, Level II, Level III
-		BidVolumeByCancelledOrder = 15,		// Level I, Level II, Level III
+		BidVolumeByCanceledOrder = 15,		// Level I, Level II, Level III
 
 		AskPriceByAddedOrder = 20,			// Level I, Level II, Level III
 		AskPriceByExecutedOrder = 21,		// Level I, Level II, Level III
 		AskPriceByCanceledOrder = 22,		// Level I, Level II, Level III
 		AskVolumeByExecutedOrder = 23,		// Level I, Level II, Level III
 		AskVolumeByAddedOrder = 24,			// Level I, Level II, Level III
-		AskVolumeByCancelledOrder = 25,		// Level I, Level II, Level III
+		AskVolumeByCanceledOrder = 25,		// Level I, Level II, Level III
 
 		BuyingVolumeByAddedOrder = 30,		// Level II, Level III
 		BuyingVolumeByCanceledOrder = 31,	// Level II, Level III
