ac1f78463fc8ad0875a2e0182fd6828eb532718a
Anton Ternavsky <anton.ternavsky@gmail.com>
2018-10-24 19:37:58 +0300

    HftMarketSettings.GridExCommission was added

--- src/Trading/Responses/Status/HftMarketSettings.cs
+++ src/Trading/Responses/Status/HftMarketSettings.cs
@@ -3,15 +3,16 @@
 namespace GridEx.API.Trading.Responses.Status
 {
 	[StructLayout(LayoutKind.Sequential, Pack = 1)]
-	public readonly struct HftMarketSettings
+	public struct HftMarketSettings
 	{
 		public HftMarketSettings(
-			double minPrice, 
-			double maxPrice, 
-			double minBidVolume, 
-			double maxBidVolume, 
-			double minAskVolume, 
-			double maxAskVolume)
+			double minPrice,
+			double maxPrice,
+			double minBidVolume,
+			double maxBidVolume,
+			double minAskVolume,
+			double maxAskVolume,
+			double gridExCommission)
 		{
 			MinPrice = minPrice;
 			MaxPrice = maxPrice;
@@ -19,13 +20,15 @@ namespace GridEx.API.Trading.Responses.Status
 			MaxBidVolume = maxBidVolume;
 			MinAskVolume = minAskVolume;
 			MaxAskVolume = maxAskVolume;
+			GridExCommission = gridExCommission;
 		}
 
-		public readonly double MinPrice;
-		public readonly double MaxPrice;
-		public readonly double MinBidVolume;
-		public readonly double MaxBidVolume;
-		public readonly double MinAskVolume;
-		public readonly double MaxAskVolume;
+		public double MinPrice;
+		public double MaxPrice;
+		public double MinBidVolume;
+		public double MaxBidVolume;
+		public double MinAskVolume;
+		public double MaxAskVolume;
+		public double GridExCommission;
 	}
 }
